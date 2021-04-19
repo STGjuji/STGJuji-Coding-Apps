83a3b5fa820653586e04c08a9ab57554df2babd0
STGjuji <kuntzoliver@gmail.com>
2021-04-19 12:28:36 +0200

    Calculator

---
+++ Calculator.bat
@@ -0,0 +1,20 @@
+@echo off
+title Batch Calculator by seJma
+color 1f
+:top
+echo --------------------------------------------------------------
+echo Welcome to Batch Calculator by STGJuji
+echo --------------------------------------------------------------
+echo.
+set /p sum=
+set /a ans=%sum%
+echo.
+echo = %ans%
+echo --------------------------------------------------------------
+pause
+cls
+echo Previous Answer: %ans%
+goto top
+pause
+exit
+
